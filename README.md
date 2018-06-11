7fde4d264d227326d611ad924a5a9c9c73318f7f
curt caldwell and eliot charette <curt@>
2018-06-11 15:11:02 -0700

    create app that acts as a to do list.

---
+++ README.md
@@ -0,0 +1,30 @@
+# _To Do List_
+
+#### _The webpage app that will keep track of tasks by receiving inputted information. 06.11.18_
+
+#### By _**Eliot Charette and Curt Caldwell**_
+
+## Description
+
+_A to do list webpage where people can add tasks to create a list of things to do. Add an option for users to indicate a task is done. Use jQuery to remove the task from the "to do" list. Make sure to identify specific behaviors your program should demonstrate, and create specifications with example input and output.._
+
+## _Setup/Installation Requirements_
+
+* _Clone from GitHub_
+* _Copy and paste from a text editor_
+* _Open Html_
+
+
+## Support and contact details
+
+_For any questions please contact EliotCharette@gmail.com or Curt.Caldwell24@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, Jquery, JavaScript, Atom, Terminal Command._
+
+### License
+
+*Licensed under MIT*
+
+Copyright (c) 2018 **_{Eliot Charette and Curt Caldwell}_**
---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//business logic
+function Place(place, time, cities, food,) {
+  this.place = place;
+  this.time = time;
+  this.cities = cities;
+  this.food = food;
+}
+
+//user interface logic
+$(function () {
+  $("form#new-place").submit(function (event) {
+    event.preventDefault();
+
+    var inputtedPlace = $("input#new-place").val();
+    var inputtedTime = $("input#new-time").val();
+    var inputtedCities = $("input#new-cities").val();
+    var inputtedFood = $("input#new-food").val();
+
+    var newPlace = new Place(inputtedPlace, inputtedTime, inputtedCities, inputtedFood);
+
+    $("ul#places").append("<li><span class='list-place'>" + newPlace.place + "</span> | <a class='delete'>Delete this task</a></li>");
+
+    $(".delete").last().click(function() {
+      $(this).parent().remove();
+      if ($("#show-place h2").text() == newPlace.place) {
+        $("#show-place").hide();
+      }
+    });
+
+    $(".list-place").last().click(function () {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.place);
+      $(".place").text(newPlace.place);
+      $(".time").text(newPlace.time);
+      $(".cities").text(newPlace.cities);
+      $(".food").text(newPlace.food);
+    });
+
+    $("input#new-place").val("");
+    $("input#new-time").val("");
+    $("input#new-cities").val("");
+    $("input#new-food").val("");
+
+  });
+});
